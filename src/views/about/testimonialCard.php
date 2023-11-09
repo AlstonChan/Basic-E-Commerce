a0ea3f0cc0ab23c43bfbb86dc02dcc68503ed695
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2023-11-09 17:12:24 +0800

    Update testimonials
    Update testimonials people image endpoints as previous endpoints provides corrupted image file

--- src/views/about/testimonialCard.php
+++ src/views/about/testimonialCard.php
@@ -1,20 +1,36 @@
-<article class="tile is-child notification is-primary">
-    <div class="media media-mobile-small-breakpoint">
-        <figure class="media-left">
-            <p class="image is-64x64">
-                <img src="<?php echo $value['avatar']; ?>">
+<?php
+$is_main = false;
+
+if (isset($index) && $index === 2) {
+    $is_main = true;
+}
+?>
+
+<article class="card <?php echo $is_main
+    ? 'max-w-fill'
+    : 'max-w-sm'; ?> bg-neutral text-primary-content mb-4 sm:mb-8 flex-1 card-small">
+    <div class="card-body">
+        <blockquote class="mb-5 font-semibold">
+            <p class="<?php echo $is_main ? 'text-xl' : ''; ?>">
+                &quot;<?php echo $value['message']; ?>&quot;
             </p>
-        </figure>
-        <div class="media-content">
-            <div class="content">
+        </blockquote>
+        <div class="flex mt-auto items-center">
+            <div class="avatar">
+                <div class="w-12 h-12 rounded-full">
+                    <img src="<?php echo $value['avatar']; ?>">
+                </div>
+            </div>
+            <div class="ml-4">
                 <p>
-                    <strong><?php echo $value[
-                        'name'
-                    ]; ?></strong> <small><?php echo $value[
-    'designation'
-]; ?></small>
-                    <br>
-                    <?php echo $value['message']; ?>
+                    <strong>
+                        <?php echo $value['name']; ?>
+                    </strong>
+                </p>
+                <p class="text-slate-300">
+                    <small>
+                        <?php echo $value['designation']; ?>
+                    </small>
                 </p>
             </div>
         </div>
--- src/views/about/testimonials.php
+++ src/views/about/testimonials.php
@@ -2,33 +2,48 @@
 
 $file = $_SERVER['DOCUMENT_ROOT'] . '/public/assets/testimonials.json';
 $data = json_decode(file_get_contents($file), true);
-
-$data = array_chunk($data, 3);
 ?>
 
 
-<section class="section has-text-centered">
-    <p class="title is-2 has-text-weight-bold">
-        Customer Review
-    </p>
-    <p class="subtitle is-4">
+<section class="mb-16 mt-12">
+    <h2 class="text-2xl md:text-4xl font-bold text-center mb-2">
+       Testimonials
+    </h2>
+    <p class="text-lg md:text-xl text-center">
         What people have said about us
     </p>
-    <div class="tile is-ancestor wide-widescreen columnsAbout">
-        <div class="tile is-parent is-vertical ">
-            <?php foreach ($data[0] as $value): ?>
-                <?php require __DIR__ . '/testimonialCard.php'; ?>
-            <?php endforeach; ?>
+    <div class="grid grid-cols-2 min-[1180px]:grid-cols-4 mt-10 min-[1180px]:gap-8 px-5 w-fit mx-auto max-w-3xl min-[1180px]:max-w-[1400px]">
+        <div class="flex flex-col col-start-1 min-[1180px]:col-start-2 row-start-1 col-span-2 w-fit">
+            <?php
+            $index = 2;
+            $value = $data[2];
+            require __DIR__ . '/testimonialCard.php';
+            unset($index);
+            ?>
+            <div class="flex gap-4 sm:gap-8">
+                <?php
+                $value = $data[3];
+                require __DIR__ . '/testimonialCard.php';
+                $value = $data[4];
+                require __DIR__ . '/testimonialCard.php';
+                ?>
+            </div>
         </div>
-        <div class="tile is-parent is-vertical">
-            <?php foreach ($data[1] as $value): ?>
-                <?php require __DIR__ . '/testimonialCard.php'; ?>
-            <?php endforeach; ?>
+        <div class="flex flex-col col-start-1 row-start-2 min-[1180px]:row-start-1 col-span-1 w-fit mr-2 sm:mr-4 min-[1180px]:mr-0">
+            <?php
+            $value = $data[0];
+            require __DIR__ . '/testimonialCard.php';
+            $value = $data[1];
+            require __DIR__ . '/testimonialCard.php';
+            ?>
         </div>
-        <div class="tile is-parent is-vertical">
-            <?php foreach ($data[2] as $value): ?>
-                <?php require __DIR__ . '/testimonialCard.php'; ?>
-            <?php endforeach; ?>
+        <div class="flex flex-col col-start-2 min-[1180px]:col-start-4 row-start-2 min-[1180px]:row-start-1 col-span-1 w-fit ml-2 sm:ml-4 min-[1180px]:ml-0">
+            <?php
+            $value = $data[5];
+            require __DIR__ . '/testimonialCard.php';
+            $value = $data[6];
+            require __DIR__ . '/testimonialCard.php';
+            ?>
         </div>
     </div>
 </section>
